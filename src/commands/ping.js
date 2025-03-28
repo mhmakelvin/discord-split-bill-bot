9957a324ea0cf4f3782a0498eed59cc73c2e7ecf
Kelvin Ma <mhmakelvin@hotmail.com>
2025-03-29 01:55:58 +0900

    Refactor

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const command = new SlashCommandBuilder()
+	.setName('ping')
+	.setDescription('Provides information about the user.')
+
+const action = async (interaction) => {
+	await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+}
+
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Provides information about the user.'),
-	async execute(interaction) {
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
-	},
+	data: command,
+	execute: action
 };
