d835e3626eeba9c5f51ee82222a03331d80cce35
Kelvin Ma <mhmakelvin@hotmail.com>
2025-04-06 05:19:05 +0900

    Change from 'firebase' to 'firebase-admin'

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,31 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 import { firebaseConfig } from "../../config.js";
+import serviceAccount from "../../serviceAccountKey.json" with { type: "json" };
 
-const firebaseApp = initializeApp(firebaseConfig);
+let firebaseAdminApp;
+let db;
 
-export const db = getFirestore(firebaseApp);
-if (process.env.STAGE === "local") {
-  connectFirestoreEmulator(db, "127.0.0.1", 8080);
+if (process.env.STAGE === "prd") {
+  firebaseAdminApp = initializeApp({
+    projectId: firebaseConfig.projectId,
+    credential: cert(serviceAccount),
+  });
+  db = getFirestore(firebaseAdminApp);
+} else {
+  firebaseAdminApp = initializeApp({
+    projectId: firebaseConfig.projectId,
+    credential: applicationDefault(),
+  });
+  db = getFirestore(firebaseAdminApp);
+
+  if (process.env.STAGE === "local") {
+    getFirestore(firebaseAdminApp).settings({
+      host: "127.0.0.1:8080",
+      ssl: false,
+    });
+    console.log("Firestore emulator connected");
+  }
 }
+
+export { db };
