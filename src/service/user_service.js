ee2a6070a1b19580b3ebe00826c01d493b5f00ce
Kelvin Ma <mhmakelvin@hotmail.com>
2025-04-21 05:00:49 +0900

    Refactoring user_service

--- src/service/user_service.js
+++ src/service/user_service.js
@@ -1,45 +1,45 @@
 import { db } from "../firebase/firebase.js";
 
-export async function isActiveUser(serverId, discordId) {
+export async function getUser(serverId, discordId) {
   const usersCollection = db.collection("users");
 
   const existingUser = await usersCollection
     .where("discordId", "==", discordId)
     .where("serverId", "==", serverId)
-    .where("active", "==", true)
     .get();
 
-  return !existingUser.empty;
+  if (existingUser.empty) return null;
+
+  return existingUser.docs[0];
+}
+
+export async function isActiveUser(serverId, discordId) {
+  const user = await getUser(serverId, discordId);
+
+  return user !== null && user.data().active;
 }
 
 export async function activateUser(serverId, discordId, name) {
   try {
-    const usersCollection = db.collection("users");
+    const user = await getUser(serverId, discordId);
 
-    const existingUser = await usersCollection
-      .where("discordId", "==", discordId)
-      .where("serverId", "==", serverId)
-      .get();
-
-    if (!existingUser.empty) {
-      existingUser.forEach((user) => {
-        user.ref.update({
-          serverId: serverId,
-          discordId: discordId,
-          name: name || discordId,
-          active: true,
-        });
-      });
-    } else {
-      const docRef = await usersCollection.add({
+    if (user !== null) {
+      user.ref.update({
         serverId: serverId,
         discordId: discordId,
         name: name || discordId,
         active: true,
       });
-      console.log("Document written with ID: ", docRef.id);
+    } else {
+      const docRef = await db.collection("users").add({
+        serverId: serverId,
+        discordId: discordId,
+        name: name || discordId,
+        active: true,
+      });
     }
   } catch (e) {
+    console.log(e);
     throw new Error("Error when activating user");
   }
 }
