b6a67a866be2656e9e7e7ff18b7cc540ff670916
Kelvin Ma <mhmakelvin@hotmail.com>
2025-04-24 01:58:56 +0900

    Update /ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,11 +1,12 @@
-import { SlashCommandBuilder } from "discord.js"
+import { SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder()
   .setName("ping")
   .setDescription("Provides information about the user.");
 
 export async function execute(interaction) {
-  await interaction.reply(
-    `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`,
-  );
-};
+  await interaction.reply({
+    content: `Discord Split Bill Bot is active!`,
+    ephemeral: true,
+  });
+}
